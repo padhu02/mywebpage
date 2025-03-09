d2c02d33cd692941c2deba1d96a19b637b7570d1
Padmanapan <padmanapananandan@gmail.com>
2025-03-09 10:41:45 +0530

    reponsive creation

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // Smooth scrolling for all anchor links
+    document.addEventListener("DOMContentLoaded", function () {
+        const checkbox = document.getElementById("check");
+        const menuLinks = document.querySelectorAll("nav ul li a");
+
+        menuLinks.forEach(link => {
+            link.addEventListener("click", () => {
+                checkbox.checked = false; // Hide menu after clicking
+            });
+        });
+    });
+
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
+});
